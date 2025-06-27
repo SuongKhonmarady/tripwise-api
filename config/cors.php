4e9731f0707da994661ca0980b5da4633945bb6a
Suong Khonmarady <maradybor@gmail.com>
2025-06-27 21:50:59 +0700

    up

---
+++ config/cors.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'allowed_methods' => ['*'],
+    'allowed_origins' => [
+        'https://www.api-scholar.site', // <-- Replace with your real frontend URL
+        'http://localhost:3000', // (optional) for local dev
+    ],
+    'allowed_origins_patterns' => [],
+    'allowed_headers' => ['*'],
+    'exposed_headers' => [],
+    'max_age' => 0,
+    'supports_credentials' => false,
+];
