904a0344d8c94d2d5f34d212000a4c87619c9eb3
Suong Khonmarady <maradybor@gmail.com>
2025-06-28 01:21:47 +0700

    update

--- config/cors.php
+++ config/cors.php
@@ -2,14 +2,18 @@
 
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
+
     'allowed_methods' => ['*'],
-    'allowed_origins' => [
-        'https://tripwise-front.vercel.app', // <-- Replace with your real frontend URL
-        'http://localhost:3000', // (optional) for local dev
-    ],
+
+    'allowed_origins' => ['https://tripwise-front.vercel.app'],
+
     'allowed_origins_patterns' => [],
+
     'allowed_headers' => ['*'],
+
     'exposed_headers' => [],
+
     'max_age' => 0,
+
     'supports_credentials' => false,
 ];
