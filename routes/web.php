0749576a3933f3679968d84ed6a1d58d0fe9c6bf
Suong Khonmarady <maradybor@gmail.com>
2025-06-27 21:06:39 +0700

    up

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/debug-pusher', function () {
-    return config('broadcasting.connections.pusher');
-});
+
